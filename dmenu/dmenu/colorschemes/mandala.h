d63e79059b61dc310ef12b1cc11849567301236b
hhacker1999 <harsh.sadh313@gmail.com>
2021-12-29 10:55:06 +0530

    use new color scheme for dmenu

---
+++ dmenu/dmenu/colorschemes/mandala.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#f1d37b", "#030203" },
+	[SchemeSel] = { "#f1d37b", "#6F5720" },
+	[SchemeOut] = { "#f1d37b", "#CF9F17" },
+};
--- dmenu/dmenu/config.def.h
+++ dmenu/dmenu/config.def.h
@@ -4,15 +4,12 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FiraCode Nerd Font:size=12"
+	"FiraCode Nerd Font:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+
+#include "colorschemes/mandala.h"
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
